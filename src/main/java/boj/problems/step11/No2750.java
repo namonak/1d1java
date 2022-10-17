1aeee9a0c5f67ed4eafa79076c414b957738aff7
namonak <namonak@gmail.com>
2022-10-17 22:12:18 +0900

    refactor: update test code and production code of No2750

--- src/main/java/boj/problems/step11/No2750.java
+++ src/main/java/boj/problems/step11/No2750.java
@@ -11,38 +11,50 @@ public class No2750 {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        solve(input, output);
+        output.write(solve(input));
 
         output.flush();
         output.close();
     }
 
-    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
-        int n = Integer.parseInt(input.readLine());
-        int arr[] = new int[n];
+    static String solve(BufferedReader input) throws IOException {
+        int number = Integer.parseInt(input.readLine());
 
+        int[] sorted = insertionSort(makeInput(input, number));
+
+        return makeOutput(number, sorted);
+    }
+
+    private static String makeOutput(int number, int[] sorted) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            sb.append(sorted[i] + "\n");
+        }
+        return sb.toString();
+    }
+
+    private static int[] makeInput(BufferedReader input, int n) throws IOException {
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(input.readLine());
         }
-
-        int[] sorted = insertionSort(arr);
-
-        for (int i = 0; i < n; i++) {
-            output.write(sorted[i] + "\n");
-        }
+        return arr;
     }
 
     private static int[] insertionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int j = i;
-
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                int tmp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = tmp;
-                j--;
-            }
+            canSwap(arr, j);
         }
         return arr;
+    }
+
+    private static void canSwap(int[] arr, int j) {
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int tmp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = tmp;
+            j--;
+        }
     }
 }
--- src/test/java/boj/problems/step11/No2750Test.java
+++ src/test/java/boj/problems/step11/No2750Test.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,20 +21,11 @@ public class No2750Test {
     void 수_정렬하기_테스트() throws IOException {
         System.out.println("수 정렬하기 : https://www.acmicpc.net/problem/2750");
 
-        BufferedReader br_given = new BufferedReader(new FileReader(input));
-        BufferedReader br_want = new BufferedReader(new FileReader(output));
-        StringWriter sw = new StringWriter();
-        BufferedWriter bw_got = new BufferedWriter(sw);
+        BufferedReader given = new BufferedReader(new FileReader(input));
+        String expected = new String(Files.readAllBytes(new File(output).toPath()));
 
-        No2750.solve(br_given, bw_got);
+        assertEquals(expected, No2750.solve(given));
 
-        bw_got.close();
-
-        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-        String got = br_got.lines().collect(Collectors.joining());
-        String want = br_want.lines().collect(Collectors.joining());
-
-        assertEquals(want, got);
+        given.close();
     }
 }

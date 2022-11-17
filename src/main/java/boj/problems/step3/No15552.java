aa85d8795bc45094ab5abfd9ac3d2570fc34f331
namonak <namonak@gmail.com>
2022-11-17 19:10:14 +0900

    refactor: update test code and production code of No15552

--- src/main/java/boj/problems/step3/No15552.java
+++ src/main/java/boj/problems/step3/No15552.java
@@ -12,17 +12,21 @@ class No15552 {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        solve(input, output);
+        output.write(solve(input));
 
+        input.close();
         output.flush();
         output.close();
     }
 
-    static void solve(BufferedReader input, BufferedWriter output) throws IOException{
+    static String solve(BufferedReader input) throws IOException{
         int testCase = Integer.parseInt(input.readLine());
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < testCase; i++) {
             StringTokenizer st = new StringTokenizer(input.readLine());
-            output.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
+            result.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
         }
+        result.setLength(result.length() - 1);
+        return result.toString();
     }
 }
--- src/test/java/boj/problems/step3/No15552Test.java
+++ src/test/java/boj/problems/step3/No15552Test.java
@@ -1,41 +1,29 @@
 package boj.problems.step3;
 
+import java.nio.file.Files;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class No15552Test {
+class No15552Test {
     File path = new File(".");
     String input = path.getAbsolutePath() + "/src/test/java/boj/problems/step3/No15552_input.txt";
     String output = path.getAbsolutePath() + "/src/test/java/boj/problems/step3/No15552_output.txt";
 
     @Test
     @DisplayName("빠른 A+B 테스트")
-    void 빠른_A_더하기_B_테스트() throws IOException {
-        BufferedReader br_given = new BufferedReader(new FileReader(input));
-        BufferedReader br_want = new BufferedReader(new FileReader(output));
-        StringWriter sw = new StringWriter();
-        BufferedWriter bw_got = new BufferedWriter(sw);
+    void test() throws IOException {
+        BufferedReader given = new BufferedReader(new FileReader(input));
+        String expected = new String(Files.readAllBytes(new File(output).toPath()));
 
-        No15552.solve(br_given, bw_got);
+        assertThat(No15552.solve(given)).isEqualTo(expected);
 
-        bw_got.close();
-
-        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-        String got = br_got.lines().collect(Collectors.joining());
-        String want = br_want.lines().collect(Collectors.joining());
-
-        assertEquals(got, want);
+        given.close();
     }
 }

c56cec9b2a40f16d648b64cbd579f91c961777b1
namonak <namonak@gmail.com>
2022-11-23 23:20:26 +0900

    refactor: update test code and production code of No2869

--- src/main/java/boj/problems/step7/No2869.java
+++ src/main/java/boj/problems/step7/No2869.java
@@ -12,22 +12,22 @@ public class No2869 {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        solve(input, output);
+        output.write(String.valueOf(solve(input)));
 
         input.close();
         output.flush();
         output.close();
     }
 
-    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+    static int solve(BufferedReader input) throws IOException {
         StringTokenizer st = new StringTokenizer(input.readLine());
         int up = Integer.parseInt(st.nextToken());
         int down = Integer.parseInt(st.nextToken());
         int length = Integer.parseInt(st.nextToken());
         int day = (length - down) / (up - down);
-        if ((length - down) % (up - down) != 0)
+        if ((length - down) % (up - down) != 0) {
             day++;
-
-        output.write(day + "\n");
+        }
+        return day;
     }
 }

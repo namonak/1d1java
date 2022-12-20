af7cebf87c9bf14e74a8aad0d39f40b19c6dc4a2
namonak <namonak@gmail.com>
2022-12-20 22:59:29 +0900

    refactor: update test code of No4948

--- src/test/java/boj/problems/step8/No4948Test.java
+++ src/test/java/boj/problems/step8/No4948Test.java
@@ -1,43 +1,33 @@
 package boj.problems.step8;
 
+import java.nio.file.Files;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class No4948Test {
+class No4948Test {
     File path = new File(".");
     String input = path.getAbsolutePath() + "/src/test/java/boj/problems/step8/No4948_input.txt";
     String output = path.getAbsolutePath() + "/src/test/java/boj/problems/step8/No4948_output.txt";
 
     @Test
     @DisplayName("베르트랑 공준 테스트")
-    void 베르트랑_공준_테스트() throws IOException {
+    void test() throws IOException {
         System.out.println("베르트랑 공준 : https://www.acmicpc.net/problem/4948");
 
-        BufferedReader br_given = new BufferedReader(new FileReader(input));
-        BufferedReader br_want = new BufferedReader(new FileReader(output));
-        StringWriter sw = new StringWriter();
-        BufferedWriter bw_got = new BufferedWriter(sw);
+        // given
+        BufferedReader given = new BufferedReader(new FileReader(input));
+        String expected = new String(Files.readAllBytes(new File(output).toPath()));
 
-        //No4948.solve(br_given, bw_got);
+        // when, then
+        //assertEquals(expected, No4948.solve(given));
 
-        bw_got.close();
-
-        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-        String got = br_got.lines().collect(Collectors.joining());
-        String want = br_want.lines().collect(Collectors.joining());
-
-        assertEquals(want, got);
+        given.close();
     }
 }

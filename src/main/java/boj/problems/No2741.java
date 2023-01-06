6ee8558c167e3ec9ab14044a9ddaae7868ed4434
namonak <namonak@gmail.com>
2023-01-06 22:57:55 +0900

    refactor: update test code and production code of No2741

--- src/main/java/boj/problems/No2741.java
+++ src/main/java/boj/problems/No2741.java
@@ -11,17 +11,21 @@ public class No2741 {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        solve(input, output);
+        output.write(solve(input) + "\n");
 
+        input.close();
         output.flush();
         output.close();
     }
 
-    static void solve(BufferedReader input, BufferedWriter output) throws IOException {
+    static String solve(BufferedReader input) throws IOException {
         int n = Integer.parseInt(input.readLine());
+        StringBuilder result = new StringBuilder();
 
         for (int i = 1; i <= n; i++) {
-            output.write(i + "\n");
+            result.append(i).append("\n");
         }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
     }
 }
--- src/test/java/boj/problems/No2741Test.java
+++ src/test/java/boj/problems/No2741Test.java
@@ -1,18 +1,14 @@
 package boj.problems;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class No2741Test {
     File path = new File(".");
@@ -24,20 +20,11 @@ public class No2741Test {
     void N_찍기_테스트() throws IOException {
         System.out.println("N 찍기 : https://www.acmicpc.net/problem/2741");
 
-        BufferedReader br_given = new BufferedReader(new FileReader(input));
-        BufferedReader br_want = new BufferedReader(new FileReader(output));
-        StringWriter sw = new StringWriter();
-        BufferedWriter bw_got = new BufferedWriter(sw);
+        BufferedReader given = new BufferedReader(new FileReader(input));
+        String expected = new String(Files.readAllBytes(new File(output).toPath()));
 
-        No2741.solve(br_given, bw_got);
+        assertThat(No2741.solve(given)).isEqualTo(expected);
 
-        bw_got.close();
-
-        BufferedReader br_got = new BufferedReader(new StringReader(sw.getBuffer().toString()));
-
-        String got = br_got.lines().collect(Collectors.joining());
-        String want = br_want.lines().collect(Collectors.joining());
-
-        assertEquals(got, want);
+        given.close();
     }
 }

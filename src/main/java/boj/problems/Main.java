188d99c3b63ecad8ad0a02ba280998da75741da9
namonak <namonak@gmail.com>
2026-03-06 11:18:49 +0900

    feat(boj): implement Main class with input handling and problem solving

---
+++ src/main/java/boj/problems/Main.java
@@ -0,0 +1,11 @@
+package boj.problems;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println(No1000.solve(input));
+  }
+}

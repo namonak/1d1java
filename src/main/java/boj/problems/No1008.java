0911a53ed5ebe37ffc674da0bb607b1e4a275744
namonak <namonak@gmail.com>
2026-03-06 14:25:53 +0900

    refactor(boj): remove main method from problem classes

--- src/main/java/boj/problems/No1008.java
+++ src/main/java/boj/problems/No1008.java
@@ -1,24 +1,10 @@
 package boj.problems;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class No1008 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        output.write(String.valueOf(solve(input)));
-
-        input.close();
-        output.flush();
-        output.close();
-    }
-
     static double solve(BufferedReader input) throws IOException {
         StringTokenizer st = new StringTokenizer(input.readLine());
         int a = Integer.parseInt(st.nextToken());
--- src/main/java/boj/problems/No1085.java
+++ src/main/java/boj/problems/No1085.java
@@ -1,23 +1,9 @@
 package boj.problems;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class No1085 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        output.write(String.valueOf(solve(input)));
-
-        input.close();
-        output.flush();
-        output.close();
-    }
-
     static int solve(BufferedReader input) throws IOException {
         String[] inputString = input.readLine().split(" ");
         int x = Integer.parseInt(inputString[0]);

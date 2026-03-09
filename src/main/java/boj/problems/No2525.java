e788ca52c1d8f5974f2bf73cf2fefb93576d22f3
namonak <namonak@gmail.com>
2026-03-09 09:21:16 +0900

    refactor(boj): remove unused imports in No2525.java

--- src/main/java/boj/problems/No2525.java
+++ src/main/java/boj/problems/No2525.java
@@ -1,10 +1,7 @@
 package boj.problems;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class No2525 {

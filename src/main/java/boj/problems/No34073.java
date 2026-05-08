4d37c07e02c52b585174f6504e45a7b06f6eda78
namonak <namonak@gmail.com>
2026-05-09 07:48:38 +0900

    fix(sonar): use No34073 word count

--- src/main/java/boj/problems/No34073.java
+++ src/main/java/boj/problems/No34073.java
@@ -6,9 +6,10 @@ import java.util.Objects;
 
 public class No34073 {
     public static String solve(BufferedReader input) throws IOException {
-        int n = Integer.parseInt(Objects.requireNonNull(input.readLine()));
-        String[] words = Objects.requireNonNull(input.readLine()).split(" ");
-        StringBuilder result = new StringBuilder();
+        int wordCount = Integer.parseInt(Objects.requireNonNull(input.readLine()));
+        String wordsLine = Objects.requireNonNull(input.readLine());
+        String[] words = wordsLine.split(" ");
+        StringBuilder result = new StringBuilder(wordsLine.length() + wordCount * "DORO ".length());
         for (String word : words) {
             result.append(word).append("DORO ");
         }

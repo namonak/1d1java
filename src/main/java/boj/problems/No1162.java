0d4a681f5394a9c165c97262895621fefb5db50d
namonak <namonak@gmail.com>
2026-06-12 13:31:44 +0900

    fix(sonar): reduce No1162 cognitive complexity

--- src/main/java/boj/problems/No1162.java
+++ src/main/java/boj/problems/No1162.java
@@ -63,58 +63,90 @@ public class No1162 {
         int m = fs.nextInt();
         int k = fs.nextInt();
 
-        List<Edge>[] graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        List<Edge>[] graph = readGraph(fs, n, m);
+        long[][] distance = initializeDistances(n, k);
+
+        return String.valueOf(findShortestPath(graph, distance, n, k));
+    }
+
+    private static List<Edge>[] readGraph(FastScanner fs, int cityCount, int roadCount)
+            throws IOException {
+        @SuppressWarnings("unchecked")
+        List<Edge>[] graph = new ArrayList[cityCount + 1];
+        for (int city = 1; city <= cityCount; city++) {
+            graph[city] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
-            int a = fs.nextInt();
-            int b = fs.nextInt();
+        for (int i = 0; i < roadCount; i++) {
+            int from = fs.nextInt();
+            int to = fs.nextInt();
             int cost = fs.nextInt();
 
-            graph[a].add(new Edge(b, cost));
-            graph[b].add(new Edge(a, cost));
+            graph[from].add(new Edge(to, cost));
+            graph[to].add(new Edge(from, cost));
         }
+        return graph;
+    }
 
-        long[][] distance = new long[n + 1][k + 1];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(distance[i], INF);
+    private static long[][] initializeDistances(int cityCount, int maxPaved) {
+        long[][] distance = new long[cityCount + 1][maxPaved + 1];
+        for (int city = 1; city <= cityCount; city++) {
+            Arrays.fill(distance[city], INF);
         }
+        return distance;
+    }
 
+    private static long findShortestPath(
+            List<Edge>[] graph, long[][] distance, int destination, int maxPaved) {
         PriorityQueue<State> pq = new PriorityQueue<>((a, b) -> Long.compare(a.cost, b.cost));
         distance[1][0] = 0L;
         pq.offer(new State(1, 0, 0L));
 
         while (!pq.isEmpty()) {
             State current = pq.poll();
+            if (current.cost != distance[current.city][current.paved]) continue;
+            if (current.city == destination) return current.cost;
 
-            if (current.cost != distance[current.city][current.paved]) {
-                continue;
-            }
-
-            if (current.city == n) {
-                return String.valueOf(current.cost);
-            }
-
-            for (Edge next : graph[current.city]) {
-                long nextCost = current.cost + next.weight;
-                if (nextCost < distance[next.to][current.paved]) {
-                    distance[next.to][current.paved] = nextCost;
-                    pq.offer(new State(next.to, current.paved, nextCost));
-                }
-
-                if (current.paved < k && current.cost < distance[next.to][current.paved + 1]) {
-                    distance[next.to][current.paved + 1] = current.cost;
-                    pq.offer(new State(next.to, current.paved + 1, current.cost));
-                }
-            }
+            relaxAdjacentRoads(graph[current.city], current, distance, maxPaved, pq);
         }
 
+        return findMinimumDistance(distance[destination]);
+    }
+
+    private static void relaxAdjacentRoads(
+            List<Edge> roads,
+            State current,
+            long[][] distance,
+            int maxPaved,
+            PriorityQueue<State> pq) {
+        for (Edge road : roads) {
+            relaxNormalRoad(road, current, distance, pq);
+            relaxPavedRoad(road, current, distance, maxPaved, pq);
+        }
+    }
+
+    private static void relaxNormalRoad(
+            Edge road, State current, long[][] distance, PriorityQueue<State> pq) {
+        long nextCost = current.cost + road.weight;
+        if (nextCost < distance[road.to][current.paved]) {
+            distance[road.to][current.paved] = nextCost;
+            pq.offer(new State(road.to, current.paved, nextCost));
+        }
+    }
+
+    private static void relaxPavedRoad(
+            Edge road, State current, long[][] distance, int maxPaved, PriorityQueue<State> pq) {
+        if (current.paved < maxPaved && current.cost < distance[road.to][current.paved + 1]) {
+            distance[road.to][current.paved + 1] = current.cost;
+            pq.offer(new State(road.to, current.paved + 1, current.cost));
+        }
+    }
+
+    private static long findMinimumDistance(long[] distances) {
         long answer = INF;
-        for (int paved = 0; paved <= k; paved++) {
-            answer = Math.min(answer, distance[n][paved]);
+        for (long distance : distances) {
+            answer = Math.min(answer, distance);
         }
-        return String.valueOf(answer);
+        return answer;
     }
 }
